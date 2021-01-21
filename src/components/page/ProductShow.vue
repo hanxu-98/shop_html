5ec2092e7a66538435248d12be3cc060763d7b51
hanxu <hx1967577835@163.com>
2021-01-21 19:29:20 +0800

    完成 商品展示

--- src/components/page/ProductShow.vue
+++ src/components/page/ProductShow.vue
@@ -11,23 +11,24 @@
       <div class="container">
         <div class="handle-box">
           <el-input  placeholder="商品名名称" class="handle-input mr10"></el-input>
-          <el-button type="primary" icon="el-icon-search">搜索</el-button>
-          <!--<el-button type="success" icon="el-icon-plus" @click="addFormFlag=true">新增</el-button>-->
+          <el-button type="primary" icon="el-icon-search" @click="queryProductTable">搜索</el-button>
+          <el-button type="success" icon="el-icon-plus" @click="toSaveHTML">新增</el-button>
         </div>
 
-        <!--<el-table
-                :data="tableData"
+        <el-table
+                :data="productShowTable"
                 border
                 class="table"
-                ref="bandTable"
+                ref="productShowTable"
                 header-cell-class-name="table-header"
-                 @selection-change="handleSelectionChange"
             >
-                <el-table-column type="selection" width="55" align="center"></el-table-column>
                 <el-table-column prop="id" label="序号" width="55" align="center"></el-table-column>
-                <el-table-column prop="name" label="品牌名" align="center"></el-table-column>
-                <el-table-column prop="bandE" label="首字母" align="center"></el-table-column>
-
+                <el-table-column prop="name" label="商品名称" align="center"></el-table-column>
+                <el-table-column prop="title" label="标题" align="center"></el-table-column>
+                <el-table-column prop="bandId" label="品牌" :formatter="bandFUN" align="center"></el-table-column>
+                <el-table-column prop="typeId" label="分类" :formatter="typeFUN" align="center"></el-table-column>
+                <el-table-column prop="productdecs" label="商品描述" align="center"></el-table-column>
+                <el-table-column prop="price" label="价格" align="center"></el-table-column>
 
                 <el-table-column prop="imgpath" label="Log(查看大图)" align="center">
                     <template slot-scope="scope">
@@ -39,19 +40,34 @@
                     </template>
                 </el-table-column>
 
-              <el-table-column prop="bandDesc" label="描述" align="center"></el-table-column>
-                <el-table-column prop="ord" label="排序" align="center"></el-table-column>
+                <el-table-column prop="stocks" label="库存" align="center"></el-table-column>
+                <el-table-column prop="sortNum" label="排序" align="center"></el-table-column>
+
+
+
+
 
                 <el-table-column label="操作" width="180" align="center">
                     <template slot-scope="scope">
-                        <el-button  type="text" icon="el-icon-edit"
+                        <!--<el-button  type="text" icon="el-icon-edit"
                             @click="bandEdit(scope.$index, scope.row)">编辑</el-button>
                         <el-button type="text" icon="el-icon-delete" class="red"
-                            @click="bandDelete(scope.$index, scope.row)">删除</el-button>
+                            @click="bandDelete(scope.$index, scope.row)">删除</el-button>-->
                     </template>
                 </el-table-column>
-            </el-table>-->
-
+            </el-table>
+            <div class="pagination">
+                <el-pagination
+                    background
+                    layout="total,sizes,prev,pager,next,jumper"
+                    :current-page="params.currentPage"
+                    :page-size="params.pageSize"
+                    :page-sizes="pageSizes"
+                    :total="totalPage"
+                    @size-change="sizeChange"
+                    @current-change="pageChange"
+                ></el-pagination>
+            </div>
 
 
       </div>
@@ -60,9 +76,83 @@
 </template>
 
 <script>
-    export default {
-        name: "ProductShow"
+  export default {
+    name: "ProductShow",
+    data() {
+      return {
+        /*查询*/
+        params: {
+          name: "",
+          currentPage: 1,
+          pageSize: 5
+        },
+        totalPage: 0,
+        pageSizes: [2, 3, 5],
+
+        /*关联数据*/
+        axiosBandId:[],
+        axiosTypeData:[],
+        productShowTable: []
+      }
+    },
+    created() {
+      this.queryProductTable();
+      this.queryBandIdData();
+      this.queryTypeData();
+    },
+    methods: {
+      toSaveHTML(){
+
+      },
+      typeFUN(row,column,cell,index){
+          for (let i = 0; i < this.axiosTypeData.length; i++) {
+            if (cell==this.axiosTypeData[i].id){
+              return this.axiosTypeData[i].name;
+            }
+        }
+      },
+      queryTypeData() {
+        this.$axios.get('http://localhost:8080/api/type/getData').then(data => {
+          this.axiosTypeData = data.data.data;  // 把请求的数据  赋给全局
+        }).catch(er => {
+          alert('查询分类异常');
+        });
+      },
+      bandFUN(row,column,cell,index){
+
+        for (let i = 0; i < this.axiosBandId.length; i++) {
+            if (cell==this.axiosBandId[i].id){
+              return this.axiosBandId[i].name;
+            }
+        }
+      },
+      queryBandIdData(){
+            this.$axios.get('http://localhost:8080/api/band/queryBandAll')
+                .then(res=>{
+                  this.axiosBandId=res.data.data;
+                }).catch(er=>{
+                  alert('查询系统异常')
+              });
+          },
+      queryProductTable() {
+
+        this.$axios.post('http://localhost:8080/api/product/queryProduct', this.$qs.stringify(this.params)).then(res => {
+          this.productShowTable = res.data.data.data;
+          this.totalPage=res.data.data.totalPage;
+        }).catch(er => {
+          alert('查询异常')
+        });
+      },
+      pageChange: function (currentPage) {
+        this.params.currentPage = currentPage;
+        this.queryProductTable();
+      },
+      sizeChange: function (pageSize) {
+        this.params.pageSize = pageSize;
+        this.queryProductTable();
+      }
     }
+  }
 </script>
 
 <style scoped>
